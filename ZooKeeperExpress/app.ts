52f5d2cb910abb8b7d25fcfbd96984a57a5c16b2
VICTOR <gurongai514@onet.pl>
2025-02-23 19:07:43 +0100

    Dodanie działającego portu 3000 oraz ukończenie rutera

---
+++ ZooKeeperExpress/app.ts
@@ -0,0 +1,8 @@
+﻿import express from 'express';
+import animalRoutes from './routes/animalRoutes';
+
+const app = express();
+app.use(express.json());
+app.use('/api', animalRoutes);
+
+export default app;
