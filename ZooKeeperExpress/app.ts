3304f4497472fd909c39ec1a8c752f4bb609ab20
VICTOR <gurongai514@onet.pl>
2025-03-01 17:09:53 +0100

    Finnal Touches

--- ZooKeeperExpress/app.ts
+++ ZooKeeperExpress/app.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', animalRoutes);
 
-// Dodaj prosty endpoint testowy
+
 app.get('/', (req, res) => {
     res.send('Server is running!');
 });
